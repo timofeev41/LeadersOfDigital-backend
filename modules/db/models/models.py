3e30990792ec712ccc7ecc01ab185b3a678aeef9
Nikolas <timofeevnik41@gmail.com>
2021-09-24 22:26:17 +0300

    :zap: new models

--- modules/db/models/models.py
+++ modules/db/models/models.py
@@ -1,3 +1,5 @@
+import typing as tp
+
 from pydantic import BaseModel
 from enum import Enum
 
@@ -14,13 +16,23 @@ class EmployeeEntry(BaseModel):
     gender: EmployeeSex
     is_married: bool
     startDate: str
-    endDate: str
-    absenceReason: str
-    absenceDays: int
+    endDate: tp.Optional[str]
+    absenceReason: tp.Optional[str]
+    absenceDays: tp.Optional[int]
     salary: int
     city: str
     childrenCount: int
 
 
-class DatabaseLinkModel(BaseModel):
-    link: str
+class FilteringClass(BaseModel):
+    speciality: tp.Optional[str]
+    birthDate: tp.Optional[tp.Tuple[str, str]]
+    gender: tp.Optional[EmployeeSex]
+    is_married: tp.Optional[bool]
+    startDate: tp.Optional[tp.Tuple[str, str]]
+    endDate: tp.Optional[tp.Tuple[str, str]]
+    absenceReason: tp.Optional[str]
+    absenceDays: tp.Optional[str]
+    salary: tp.Optional[tp.Tuple[int, int]]
+    city: tp.Optional[str]
+    childrenCount: tp.Optional[int]
