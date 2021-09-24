b03e74a313e88c90df4832c6b3824ff7403868c6
Nikolas <timofeevnik41@gmail.com>
2021-09-24 21:32:00 +0300

    :zap: api endpoints

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ import typing as tp
 
 from fastapi import FastAPI
 
-from modules.db.models.models import EmployeeEntry
-
 api = FastAPI()
 
 fake_data = [
     {"id": "23854332", "speciality": "Ведущий инженер", "salary": 38600},
     {"id": "23854336", "speciality": "Машинист", "salary": 25200},
-    {"id": "23854539", "speciality": "заместитель главного инженера", "salary":151100},
+    {"id": "23854539", "speciality": "заместитель главного инженера", "salary": 151100},
     {"id": "23154332", "speciality": "based dep", "salary": 50000},
     {"id": "23854336", "speciality": "Заринус", "salary": 2500},
     {"id": "23854139", "speciality": "Андрюхус", "salary": 7500},
@@ -20,6 +18,18 @@ fake_data = [
 ]
 
 
-@api.get("/api/filter", response_model=tp.List[EmployeeEntry])
-async def filter_employees(start: int, end: int) -> tp.Dict[str, tp.Any]:
-    pass
+@api.get("/api/employees")
+async def filter_employees(start: tp.Optional[int], end: tp.Optional[int]):
+    if not start or not end:
+        return fake_data
+    result: tp.List[tp.Dict[str, tp.Any]] = []
+    for data in fake_data:
+        if start <= data["salary"] <= end:
+            result.append(data)
+    return result
+
+
+@api.post("/api/employees")
+async def create_employee(id: str, speciality: str, salary: int):
+    fake_data.append({"id": id, "speciality": speciality, "salary": salary})
+    return {"id": id, "speciality": speciality, "salary": salary}
