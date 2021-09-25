385f7c22fd88a31e70449bd81abeaf38859ce702
Olga Lipina <neon_alfa@mail.ru>
2021-09-26 01:59:54 +0300

    Viable Model. Regressor

--- modules/ml/predictor_class.py
+++ modules/ml/predictor_class.py
@@ -1,7 +1,8 @@
 from joblib import load
 import pandas as pd
 import numpy as np
-from .training import preprocess
+#from .training import preprocess
+from training import preprocess
 
 # some_data_passed
 
@@ -23,9 +24,9 @@ class Analitics:
         df2 = self.preprocess(df2)
         X = df2.drop("workingPeriod", axis=1)
 
-        start_dates = pd.to_datetime(pd.Series(self.df["startDate"].values, index=self.df.index), format="%Y-%m-%d")
+        start_dates = pd.to_datetime(pd.Series(self.df["startDate"].values, index=self.df.index), format="%d/%m/%Y")
 
-        predictions = self.model.predict(X)
+        predictions = np.round(self.model.predict(X))
 
         left_days = (
             (
@@ -35,5 +36,102 @@ class Analitics:
             ).dt.days
         ).apply(lambda x: max(x, 0))
 
-        return left_days
+        return left_days.values
 
+
+
+if __name__ == "__main__":
+    class EmployeeEntry():
+        def __init__(self,
+                     id,
+                     speciality,
+                     birthDate,
+                     education,
+                     gender,
+                     is_married,
+                     startDate,
+                     endDate,
+                     absenceReason,
+                     absenceDays,
+                     salary,
+                     city,
+                     childrenCount,
+                     is_fired,
+                     mentored,
+                     ):
+            self.id = id
+            self.speciality = speciality
+            self.birthDate = birthDate
+            self.education = education
+            self.gender = gender
+            self.is_married = is_married
+            self.startDate = startDate
+            self.endDate = endDate
+            self.absenceReason = absenceReason
+            self.absenceDays = absenceDays
+            self.salary = salary
+            self.city = city
+            self.childrenCount = childrenCount
+            self.is_fired = is_fired
+            self.mentored = mentored
+
+    instance = EmployeeEntry(id=108,
+                             speciality='Ведущий инженер',
+                             birthDate='02/07/2002',
+                             education='school',
+                             gender="male",
+                             is_married=True,
+                             startDate='02/07/2018',
+                             endDate='15/10/2024',
+                             absenceReason=None,
+                             absenceDays=None,
+                             salary=38900,
+                             city='Москва',
+                             childrenCount=2,
+                             is_fired=True,
+                             mentored=False
+
+                             )
+    instance2 = EmployeeEntry(id=101,
+                              speciality='Уборщик',
+                              birthDate='02/07/2012',
+                              education='higher',
+                              gender="female",
+                              is_married=True,
+                              startDate='02/07/2019',
+                              endDate='10/10/2022',
+                              absenceReason='Ilness',
+                              absenceDays=2,
+                              salary=3900,
+                              city='Москва',
+                              childrenCount=2,
+                              is_fired=True,
+                              mentored=True
+
+                              )
+
+    instance3 = EmployeeEntry(id=102,
+                              speciality='Уборщик',
+                              birthDate='02/07/2008',
+                              education='college',
+                              gender="female",
+                              is_married=False,
+                              startDate='02/07/2019',
+                              endDate='10/10/2023',
+                              absenceReason='Holiday',
+                              absenceDays=5,
+                              salary=18000,
+                              city='Припять',
+                              childrenCount=10,
+                              is_fired=True,
+                              mentored=False
+
+                              )
+
+
+
+
+
+    employees = [instance, instance2, instance3]
+    predictor = Analitics(employees)
+print(predictor.predict())
