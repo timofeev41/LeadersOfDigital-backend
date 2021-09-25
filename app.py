89aa2b3306dfc668b89328934db62c0c8fad340b
Nikolas <timofeevnik41@gmail.com>
2021-09-25 14:34:59 +0300

    :zap: bugfix

--- app.py
+++ app.py
@@ -8,24 +8,11 @@ from modules.utils.ml import BaseClassifier
 
 api = FastAPI()
 
-fake_data = [
-    {"id": "23854332", "speciality": "Ведущий инженер", "salary": 38600},
-    {"id": "23854336", "speciality": "Машинист", "salary": 25200},
-    {"id": "23854539", "speciality": "заместитель главного инженера", "salary": 151100},
-    {"id": "23154332", "speciality": "based dep", "salary": 50000},
-    {"id": "23854336", "speciality": "Заринус", "salary": 2500},
-    {"id": "23854139", "speciality": "Андрюхус", "salary": 7500},
-    {"id": "21852332", "speciality": "база", "salary": 12333},
-    {"id": "23852336", "speciality": "Антонус", "salary": 500},
-    {"id": "23853539", "speciality": "Егорус", "salary": 1200},
-    {"id": "23844332", "speciality": "Приколист", "salary": 12005},
-]
-
 
 @api.get("/api/employees")
-async def get_employees(count: int = 100):
+async def get_employees(start: int = 0, count: int = 100):
     employees = await MongoDbWrapper().get_all_employees()
-    return employees[:100]
+    return employees[start:count]
 
 
 @api.post("/api/employees/filter")
@@ -37,9 +24,13 @@ async def filter_employees(filter: FilteringClass):
 
 @api.post("/api/employees")
 async def create_employee(employee: EmployeeEntry):
-    data = dict(employee)
-    fake_data.append(data)
-    return data
+    await MongoDbWrapper().push_employee_to_collection([dict(employee)])
+    return {"status": True}
+
+
+@api.patch("/api/upload")
+async def add_education():
+    await MongoDbWrapper().update_employee()
 
 
 @api.patch("/api/employee")
--- modules/db/db.py
+++ modules/db/db.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import random
 import typing as tp
 
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection, AsyncIOMotorCursor
@@ -72,3 +73,10 @@ class MongoDbWrapper(metaclass=SingletonMeta):
 
     async def get_all_employees(self) -> tp.List[tp.Dict[str, tp.Any]]:
         return await self._execute_all_from_collection(self._employees_data)
+
+    async def update_employee(self, key: str = None, value: str = None):
+        data = await self._execute_all_from_collection(self._employees_data)
+        for doc in data:
+            await self._employees_data.update_one(
+                {"id": doc["id"]}, {"$set": {"education": random.choice(["college", "higher", "school"])}}
+            )
